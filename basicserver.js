4a62b5dce4e5f9f043dddda6b5f222c756773f99
artibansode <artib@sies.edu.in>
2025-09-03 20:34:44 +0530

    First commit: added server.js

---
+++ basicserver.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from Node.js HTTP Server\n');
+});
+
+// Listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
